431ce7d0840415d9aace0511a16422f5505238a7
Sabrina Kennedy <skenne21@gmail.com>
2017-12-30 11:43:45 -0700

    Set importance feature working

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@ $('#save-btn').on('click', userData);
 $('.input-task').on('keyup', enableBtn);
 $('.input-title').on('keyup', enableBtn);
 $('.append-here').on('click', '#delete-btn', removeCard);
-// $('.append-here').on('click', '#upvote-btn', setQuality);
-// $('.append-here').on('click', '#downvote-btn', setQuality);
+$('.append-here').on('click', '#upvote-btn', setImportance);
+$('.append-here').on('click', '#downvote-btn', setImportance);
 $('.append-here').on('blur', 'p', editTask);
 $('.append-here').on('blur', 'h3', editTitle);
 $('#search-field').on('keyup', searchCards)
@@ -13,12 +13,12 @@ $('.append-here').on('click', '.completed-btn', completedValue);
 $('.completed-tasks').on('click', showCompleted);
 $('.more-tasks').on('click', getCardFromStoreage);
 
-function NewCard (title, task, id, quality, completed){
+function NewCard (title, task, id, importance, completed){
   this.title = title;
   this.task = task;
   this.id = id;
-  this.quality = quality || ' swill';
-  this.voteCounter = 0; 
+  this.importance = importance || ' Normal';
+  this.importanceCounter = 2; 
   this.completed = false;
 }
 
@@ -29,7 +29,7 @@ NewCard.prototype.prependCard = function() {
     <p contenteditable=true>${this.task}</p>
     <button class="card-button bottom-line" id="upvote-btn"></button>
     <button class="card-button bottom-line" id="downvote-btn"></button>
-    <h6 class="bottom-line">quality: <span class="quality-change">${this.quality}</span></h6>
+    <h6 class="bottom-line">Importance: <span class="importance-change">${this.importance}</span></h6>
     <button class="completed-btn">Mark as Completed</button>
     </article>`);
 };
@@ -75,7 +75,7 @@ function getCardFromStoreage() {
   var retrieveCard = localStorage.getItem(localStorage.key(i));
   var parsedCard = JSON.parse(retrieveCard);
   if (parsedCard.completed === false) {
-    var refreshCard = new NewCard (parsedCard.title, parsedCard.task, parsedCard.id, parsedCard.quality);
+    var refreshCard = new NewCard (parsedCard.title, parsedCard.task, parsedCard.id, parsedCard.importance);
     refreshCard.prependCard();
     }
   }
@@ -87,75 +87,58 @@ function removeCard() {
   localStorage.removeItem(idRemoved);
 }
 
-// function setQuality() {
-//   var key = $(this).parent().attr('id');
-//   var retrieveObject = localStorage.getItem(key);
-//   var parseObject = JSON.parse(retrieveObject);
-//   if( event.target.id === 'upvote-btn' && parseObject.voteCounter < 3){
-//     parseObject.voteCounter++;
-//     console.log(parseObject.voteCounter)
-//   }
-  // var qualityText = $(this).siblings('h6').children('span').text();
-  // var qualityArray = ['swill', 'plausible', 'genius'];
-  // var button = $(this);
-  // changeQuality(button, parseObject, qualityArray, qualityText);
-// }
-
-function changeUpvote() {
+function setImportance(e) {
   var key = $(this).parent().attr('id');
   var retrieveObject = localStorage.getItem(key);
-  var parseObject = JSON.parse(retrieveObject);
+  var parsedObject = JSON.parse(retrieveObject);
   var htmlText = $(this).siblings('h6').children('span');
-  if(htmlText.text() === ' swill') {
-    htmlText.text(' plausible');
-    parseObject.quality = ' plausible';
-    var stringifyObject = JSON.stringify(parseObject);
-    localStorage.setItem(key, stringifyObject);
-    } else if(htmlText.text() === ' plausible') {
-    htmlText.text(' genius');
-    parseObject.quality = 'genius';
-    var stringifyObject = JSON.stringify(parseObject);
-    localStorage.setItem(key, stringifyObject);
-  };
-};
+  setImportanceCounter(e, parsedObject, htmlText, key);
+}
 
-function changeDownvote() {
-  console.log(this)
-  var key = $(this).parent().attr('id');
-  var retrieveObject = localStorage.getItem(key);
-  var parseObject = JSON.parse(retrieveObject);
-  var htmlText = $(this).siblings('h6').children('span');
-  if(htmlText.text() === 'genius') {
-    htmlText.text(' plausible');
-    parseObject.quality = ' plausible';
-    var stringifyObject = JSON.stringify(parseObject);
+function setImportanceCounter(e, parsedObject, htmlText, key) { 
+  if (e.target.id === 'upvote-btn' && parsedObject.importanceCounter < 4 ) {
+    parsedObject.importanceCounter++;
+    var stringifyObject = JSON.stringify(parsedObject);
     localStorage.setItem(key, stringifyObject);
-  } else if(htmlText.text() === ' plausible') {
-    htmlText.text(' swill');
-    parseObject.quality = 'swill';
-    var stringifyObject = JSON.stringify(parseObject);
-    console.log(stringifyObject)
-    localStorage.setItem(key, stringifyObject)
-  };
-};
+    changeImportanceText(parsedObject, htmlText, key)
+  } else if (e.target.id === 'downvote-btn' && (parsedObject.importanceCounter > 0)) {
+    parsedObject.importanceCounter--;
+    var stringifyObject = JSON.stringify(parsedObject);
+    localStorage.setItem(key, stringifyObject);
+    changeImportanceText(parsedObject, htmlText, key);
+  }
+}
+
+function changeImportanceText(parsedObject, htmlText, key) {
+  console.log(htmlText)
+  var importanceArray = ['None', 'Low', 'Normal', 'High', 'Critical'];
+  importanceArray.forEach(function(value, index) {
+    if (parsedObject.importanceCounter  === index) {
+      parsedObject.importance = value;
+      htmlText.text(value);
+      var stringifyObject = JSON.stringify(parsedObject);
+      localStorage.setItem(key, stringifyObject);
+    }
+  })
+}
 
 function editTitle() {
   var cardId = $(this).parent().attr('id');
   var storedId = localStorage.getItem(cardId);
-  var parseObject = JSON.parse(storedId);
+  var parsedObject = JSON.parse(storedId);
   var titleText = $(this).text();
-  parseObject.title = titleText;
-  var stringTitle = JSON.stringify(parseObject);
+  parsedObject.title = titleText;
+  var stringTitle = JSON.stringify(parsedObject);
   localStorage.setItem(cardId, stringTitle);
 };
 
 function editTask() {
   var cardId = $(this).parent().attr('id');
   var storedId = localStorage.getItem(cardId);
-  var parseObject = JSON.parse(storedId);
+  var parsedObject = JSON.parse(storedId);
   var taskText = $(this).text();
-  parseObject.task = taskText;
-  var stringTask = JSON.stringify(parseObject);
+  parsedObject.task = taskText;
+  var stringTask = JSON.stringify(parsedObject);
   localStorage.setItem(cardId, stringTask);
 };
 
@@ -164,9 +147,9 @@ function searchCards() {
   for (var i = 0; i <localStorage.length; i++) {
     var key = localStorage.key(i);
     var pulledObject = localStorage.getItem(key);
-    var parseObject = JSON.parse(pulledObject);
-    if (parseObject.title.includes($(this).val()) || parseObject.task.includes($(this).val())) {
-      var cardId = '#' + parseObject.id;
+    var parsedObject = JSON.parse(pulledObject);
+    if (parsedObject.title.includes($(this).val()) || parsedObject.task.includes($(this).val())) {
+      var cardId = '#' + parsedObject.id;
       $(cardId).removeClass('hidden');
     };
   };
@@ -180,14 +163,14 @@ function toggleCompletedAppearance() {
 function completedValue() {
   var cardId = $(this).parent().attr('id');
   var storedId = localStorage.getItem(cardId);
-  var parseObject = JSON.parse(storedId);
+  var parsedObject = JSON.parse(storedId);
   if($(this).closest('article').hasClass('completed-card') === true) {
-    parseObject.completed = true;
-    var stringTask = JSON.stringify(parseObject);
+    parsedObject.completed = true;
+    var stringTask = JSON.stringify(parsedObject);
     localStorage.setItem(cardId, stringTask);
   } else {
-    parseObject.completed = false;
-    var stringTask = JSON.stringify(parseObject);
+    parsedObject.completed = false;
+    var stringTask = JSON.stringify(parsedObject);
     localStorage.setItem(cardId, stringTask);
   }
 }
@@ -195,26 +178,27 @@ function completedValue() {
 function showCompleted() {
   $('.cards').addClass('hidden');
   for(var i = 0; i < localStorage.length; i++){
-  var retrieveCard = localStorage.getItem(localStorage.key(i));
-  var parsedCard = JSON.parse(retrieveCard);
-  if(parsedCard.completed === true) {
+    var retrieveCard = localStorage.getItem(localStorage.key(i));
+    var parsedCard = JSON.parse(retrieveCard);
+    if(parsedCard.completed === true) {
     // $('.cards').toggleClass('.completed-card');
-    var refreshCard = new NewCard (parsedCard.title, parsedCard.task, parsedCard.id, parsedCard.quality);
+    var refreshCard = new NewCard (parsedCard.title, parsedCard.task, parsedCard.id, parsedCard.importance);
     refreshCard.prependCard();
+    }
   }
-}}
+}
 
 function displayTen() {
   var newArray = [];  
-    for(var i = 0; i < localStorage.length; i++){
+  for(var i = 0; i < localStorage.length; i++){
     newArray.push(localStorage.key(i));
     var sortedArray = newArray.sort(function(a, b) {
      return a - b;
-    });
-    var retrieveCard = localStorage.getItem(localStorage.key(i));
-    var parsedCard = JSON.parse(retrieveCard);
-    if (newArray.length <= 10 && parsedCard.completed === false){
-    var refreshCard = new NewCard (parsedCard.title, parsedCard.task, parsedCard.id, parsedCard.quality);
+  });
+  var retrieveCard = localStorage.getItem(localStorage.key(i));
+  var parsedCard = JSON.parse(retrieveCard);
+  if (newArray.length <= 10 && parsedCard.completed === false){
+    var refreshCard = new NewCard (parsedCard.title, parsedCard.task, parsedCard.id, parsedCard.importance);
     refreshCard.prependCard();
     }
   }
